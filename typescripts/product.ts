c0b1a8e6d9f8568af935f198d75e76fe2d4e7f68
Scyllizzy <babytink2002@hotmail.com>
2019-11-12 13:46:47 -0800

    Created a product class

---
+++ typescripts/product.ts
@@ -0,0 +1,22 @@
+/**Represents a single shopping cart item.*/
+class Product {
+    /** Name of the product.*/
+    name:string;
+    /** Description of the product.*/
+    description:string;
+    /** Price of the product.*/
+    price:number;
+}
+
+
+
+
+//Test code
+{
+/*  let prod = new Product();
+
+    prod.name = "something";
+    prod.description = "other things";
+    prod.price = 10.99;
+*/
+}
