c69b873657d43b33b74b93fa8dde64732da80e69
Scyllizzy <babytink2002@hotmail.com>
2019-11-14 14:39:36 -0800

    Created a display for products when shopping cart is clicked

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,8 @@
 window.onload = function () {
     initBuyBtns();
+    displayNumberOfItems();
+    var cartIcon = document.querySelector("div#shopping-cart");
+    cartIcon.onclick = showCartContents;
 };
 /**
  * Wire up all the "Buy" buttons to an onclick event.
@@ -11,15 +14,19 @@ function initBuyBtns() {
         currBtn.onclick = buyProduct;
     }
 }
+/**
+ * Adds the product that was clicked to the shopping cart and saves it to local storage.
+ */
 function buyProduct() {
-    var prod = getProduct();
+    var currBtn = this; // The "Buy" button that was clicked
+    var prod = getProduct(currBtn);
     saveProductToCart(prod);
+    displayNumberOfItems();
 }
 /**
  * Get the product object for the currently selected product.
  */
-function getProduct() {
-    var currBuyBtn = this;
+function getProduct(currBuyBtn) {
     var currProdDiv = currBuyBtn.parentElement;
     var prod = new Product();
     prod.name = currProdDiv.querySelector(".title").innerHTML;
@@ -29,7 +36,47 @@ function getProduct() {
     prod.price = parseFloat(price);
     return prod;
 }
+/**
+ * Saves the product to the shopping cart in local storage.
+ * @param p The product to be added.
+ */
 function saveProductToCart(p) {
+    ProductStorage.addProduct(p);
+    return ProductStorage.getAllProducts();
+}
+/**
+ * Displays the number of products in the shopping cart.
+ */
+function displayNumberOfItems() {
+    var numItems = ProductStorage.getNumOfProducts();
+    var cartSpan = document.querySelector("div#shopping-cart > span");
+    cartSpan.innerHTML = numItems.toString();
+}
+function showCartContents() {
+    var displayDiv = document.querySelector("#display-cart");
+    displayDiv.innerHTML = "";
+    var allProds = ProductStorage.getAllProducts();
+    for (var i = 0; i < allProds.length; i++) {
+        var prod = allProds[i];
+        //Create a div for each product
+        var prodDiv = document.createElement("div");
+        //Add a class for that div
+        prodDiv.classList.add("display-product");
+        //Create the h2
+        var h2 = document.createElement("h2");
+        //Add title w/ product name & price to the innerHTML of the h2
+        h2.innerHTML = prod.name + " - $" + prod.price;
+        //Create the paragraph
+        var description = document.createElement("p");
+        //Add the product description to the paragraph innerHTML 
+        description.innerHTML = prod.description;
+        //Add the h2 as a child element to the product div
+        prodDiv.appendChild(h2);
+        //Add the p as a child element to the product div under the h2
+        prodDiv.appendChild(description);
+        //Add the whole product to the display div for all the products
+        displayDiv.appendChild(prodDiv);
+    }
 }
 /**Represents a single shopping cart item.*/
 var Product = /** @class */ (function () {
@@ -46,3 +93,33 @@ var Product = /** @class */ (function () {
         prod.price = 10.99;
     */
 }
+var ProductStorage = /** @class */ (function () {
+    function ProductStorage() {
+    }
+    // Add Product
+    ProductStorage.addProduct = function (p) {
+        // Get exsisting products from local storage
+        var prods = ProductStorage.getAllProducts();
+        // Add the product
+        prods.push(p);
+        // Turn it into a JSON string again
+        var data = JSON.stringify(prods);
+        // Re-add it back to local storage
+        localStorage.setItem("prods", data);
+    };
+    // Retrieve all products
+    /**
+     * Returns all products, or returns an empty array if no products are stored.
+     */
+    ProductStorage.getAllProducts = function () {
+        var data = localStorage.getItem("prods");
+        if (data == null)
+            return new Array();
+        return JSON.parse(data);
+    };
+    // Get number of products
+    ProductStorage.getNumOfProducts = function () {
+        return ProductStorage.getAllProducts().length;
+    };
+    return ProductStorage;
+}());
--- typescripts/main.ts
+++ typescripts/main.ts
@@ -1,5 +1,9 @@
 window.onload = function() {
     initBuyBtns();
+    displayNumberOfItems();
+
+    let cartIcon = <HTMLElement>document.querySelector("div#shopping-cart");
+    cartIcon.onclick = showCartContents;
 }
 
 /**
@@ -14,16 +18,20 @@ function initBuyBtns() {
     }
 }
 
+/**
+ * Adds the product that was clicked to the shopping cart and saves it to local storage.
+ */
 function buyProduct() {
-    let prod = getProduct();
+    let currBtn = this; // The "Buy" button that was clicked
+    let prod = getProduct(currBtn);
     saveProductToCart(prod);
+    displayNumberOfItems();
 }
 
 /**
  * Get the product object for the currently selected product. 
  */
-function getProduct() {
-    let currBuyBtn = <HTMLElement>this;
+function getProduct(currBuyBtn:HTMLElement) {
     let currProdDiv = currBuyBtn.parentElement;
 
     let prod = new Product();
@@ -37,6 +45,54 @@ function getProduct() {
     return prod;
 }
 
+/**
+ * Saves the product to the shopping cart in local storage.
+ * @param p The product to be added.
+ */
 function saveProductToCart(p:Product):Product[] {
+    ProductStorage.addProduct(p);
+    return ProductStorage.getAllProducts();
+}
 
+/**
+ * Displays the number of products in the shopping cart.
+ */
+function displayNumberOfItems() {
+    let numItems = ProductStorage.getNumOfProducts();
+    let cartSpan = document.querySelector("div#shopping-cart > span");
+    cartSpan.innerHTML = numItems.toString();
+}
+
+function showCartContents() {
+    let displayDiv = document.querySelector("#display-cart");
+    displayDiv.innerHTML = "";
+
+    let allProds = ProductStorage.getAllProducts();
+
+    for (let i = 0; i < allProds.length; i++) {
+        const prod = allProds[i];
+
+        //Create a div for each product
+        let prodDiv = document.createElement("div");
+        //Add a class for that div
+        prodDiv.classList.add("display-product");
+
+        //Create the h2
+        let h2 = document.createElement("h2");
+        //Add title w/ product name & price to the innerHTML of the h2
+        h2.innerHTML = prod.name + " - $" + prod.price;
+
+        //Create the paragraph
+        let description = document.createElement("p");
+        //Add the product description to the paragraph innerHTML 
+        description.innerHTML = prod.description;
+
+        //Add the h2 as a child element to the product div
+        prodDiv.appendChild(h2);
+        //Add the p as a child element to the product div under the h2
+        prodDiv.appendChild(description);
+
+        //Add the whole product to the display div for all the products
+        displayDiv.appendChild(prodDiv);
+    }
 }
